4cae5b7ab10b6f8787dba1e3efecffb72dbdb8c3
C-r0 <146779565+C-r0@users.noreply.github.com>
2026-01-24 12:17:13 -0300

    Add files via upload

--- boot.asm
+++ boot.asm
@@ -1,60 +1,55 @@
 [bits 16]
-[org 0x7c00]
+[org 0x7C00]
 
-    mov [BOOT_DRIVE], dl ; Salva o drive de boot logo no início
-
+start:
+    mov [BOOT_DRIVE], dl
+    cli
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7c00
+    mov sp, 0x7C00
+    sti
 
-    ; --- RESET DISK ---
-    xor ax, ax          ; ah = 0 (reset disk), al = 0
+    ; A20
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    ; load stage2 (25 setores) em 0x1000
+    xor ax, ax
+    mov es, ax
+    mov bx, 0x1000
+
+    mov ah, 0x02
+    mov al, 25
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
     mov dl, [BOOT_DRIVE]
     int 0x13
+    jc $
 
-    ; --- LOAD PM.BIN ---
-    mov ah, 0x02        ; READ SECTOR
-    mov al, 1           ; 32 SECTOR (pm.bin)
-    mov ch, 0           
-    mov cl, 2           ; SECTOR 2 
-    mov dh, 0           ; HEAD 0
-    mov dl, [BOOT_DRIVE]
-    mov bx, 0x1000      
-    int 0x13
-    jc disk_error       
-
-
-    mov ah, 0x0e
-    mov al, 'L'
-    int 0x10
-
-    ; PROTECTED MODE
     cli
     lgdt [gdt_descriptor]
+
     mov eax, cr0
     or eax, 1
     mov cr0, eax
 
     jmp 0x08:0x1000
 
-disk_error:
-    mov ah, 0x0e
-    mov al, 'E'
-    int 0x10
-    jmp $
+BOOT_DRIVE db 0
 
-BOOT_DRIVE db 0 
-
-; --- GDT AND SING ---
-gdt_start: dq 0x0
-gdt_code:  dw 0xFFFF, 0x0000, 0x9A00, 0x00CF 
-gdt_data:  dw 0xFFFF, 0x0000, 0x9200, 0x00CF
+gdt_start:
+    dq 0
+    dq 0x00CF9A000000FFFF
+    dq 0x00CF92000000FFFF
 gdt_end:
+
 gdt_descriptor:
     dw gdt_end - gdt_start - 1
     dd gdt_start
 
 times 510-($-$$) db 0
-dw 0xaa55
+dw 0xAA55
